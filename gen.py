735382eedd53ce13a1147f5c0ec7b268fe75f0e0
stevesan <stevenan@gmail.com>
2015-06-21 09:00:06 -0400

    cleanup

--- gen.py
+++ gen.py
@@ -3,9 +3,11 @@ import numpy
 import networkx as nx
 import pylab
 import math
+import scipy
+from scipy.cluster.vq import kmeans
 import sys
 from planar import Vec2
-
+from vec2utils import *
 
 nbor4dx = [1, 0, -1, 0]
 nbor4dy = [0, 1, 0, -1]
@@ -13,78 +15,6 @@ nbor4dy = [0, 1, 0, -1]
 nbor8dx = [1, 1, 0, -1, -1, -1, 0, 1]
 nbor8dy = [0, 1, 1, 1, 0, -1, -1, -1]
 
-class Int2:
-    x = 0
-    y = 0
-
-    def __init__(self, _x, _y):
-        self.x = _x
-        self.y = _y
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def yieldtest(self):
-        yield 1
-        yield 2
-
-    def yield_4nbors(self):
-        """ in ccw order """
-        p = self
-        for i in range(4):
-            yield Int2(p.x+nbor4dx[i], p.y+nbor4dy[i])
-
-    def yield_8nbors(self):
-        """ in ccw order """
-        p = self
-        for i in range(8):
-            yield Int2(p.x+nbor8dx[i], p.y+nbor8dy[i])
-
-    def yield_8nbors_rand(self):
-        """ in random order """
-        p = self
-        for i in numpy.random.permutation(8):
-            yield Int2(p.x+nbor8dx[i], p.y+nbor8dy[i])
-
-    def astuple(self): return (self.x, self.y)
-
-class Grid:
-    W = 1
-    H = 1
-    grid = None
-
-    def __init__(self,_W, _H, default):
-        self.W = _W
-        self.H = _H
-        self.grid = range(self.W*self.H)
-        for i in range(self.W*self.H):
-            self.grid[i] = default
-
-    def check(self,p):
-        return p.x >= 0 and p.x < self.W and p.y >= 0 and p.y < self.H
-
-    def get(self,x,y):
-        return self.grid[self.W*y + x]
-
-    def pget(self,p):
-        return self.grid[self.W*p.y + p.x]
-
-    def set(self,x,y,value):
-        self.grid[self.W*y+x] = value
-
-    def pset(self,p,value):
-        self.grid[self.W*p.y+p.x] = value
-
-    def printgrid(self):
-        for y in range(self.H):
-            for x in range(self.W):
-                print self.get(x,y),
-            print ''
-
-    def iter(self):
-        for y in range(self.W):
-            for x in range(self.H):
-                yield (x,y)
 
 def testBasic():
     g = Grid(80,80,' ')
@@ -96,27 +26,14 @@ def testBasic():
     g.printgrid()
 
 
-def gaussian(frac, stdev):
-    return math.exp(-1 * (frac*frac)/(stdev*stdev))
-
-class InterestCurve:
-    firstBump = 0.5
-    lastBump = 1.0
-    def eval(self, frac):
-        rv = 0
-        rv += self.firstBump * gaussian(frac-0.1, 0.10)
-        for center in [ 0.3, 0.5, 0.7]:
-            rv += (center*0.8) * 1.0 * gaussian(frac-center, 0.075)
-        rv += 1.0 * gaussian(frac-0.95, 0.1)
-        return rv
-
+    """
 curve = InterestCurve()
 ff = range(200)
 ii = range(200)
 for i in range(len(ff)):
     ff[i] = i*1.0/(len(ff)-1)
-# ii[i] = gaussian(ff[i], 0.2)
     ii[i] = curve.eval(ff[i])
+    """
 
 # pylab.plot(ff,ii,'.-')
 # pylab.xlim([0, 1])
@@ -151,45 +68,47 @@ def tunnel(g, p, c, numdigs, maxdigs):
 # g.printgrid()
 
 G = nx.Graph()
-NV = 20
-numGroups = 3
+NV = 35
+numGroups = 4
 groupSize = NV/numGroups
 
-nodePos = {}
+nodePos = slow_poisson_sampler(0.1, NV)
+
+for u in range(len(nodePos)):
+    G.add_node(u)
+
+"""
 for i in range(NV):
     G.add_node(i)
-    nodePos[i] = Vec2(random.random(), random.random())
+    nodePosDict[i] = Vec2(random.random(), random.random())
+    """
 
 # compute distances
-D = Grid(NV,NV, 0.0)
+
+nodeGroup = [-1 for u in range(NV)]
+
+"""
 for u in range(NV):
-    for v in range(NV):
-        pu = nodePos[u]
-        pv = nodePos[v]
-        D.set(u,v, (pu-pv).length)
-
-nodeGroup = {}
-
-def key_func(v):
-    if v in nodeGroup:
-        return sys.float_info.max
-    else:
-        return D.get
-
-for u in range(NV):
-    if u in nodeGroup: continue
+    if nodeGroup[u] != -1: continue
     nbors = range(NV)
-    nbors.sort(key=lambda v: D.get(u,v) if v not in nodeGroup else sys.float_info.max)
+    nbors.sort(key=lambda v: D.get(u,v) if nodeGroup[v] == -1 else sys.float_info.max)
     for i in range(0,groupSize+1):
         v = nbors[i]
-        if v in nodeGroup: continue
+        if nodeGroup[v] != -1: continue
         nodeGroup[v] = u
         print u,v
         G.add_edge(u,v)
+"""
 
-drawPos = {}
-for node in nodePos:
-    vec = nodePos[node]
-    drawPos[node] = (vec.x, vec.y)
-nx.draw(G, drawPos)
+tupleArray = [(p.x, p.y) for p in nodePos]
+posMatrix = numpy.matrix(tupleArray)
+print posMatrix
+# TODO call whiten here??
+(centroids, distortion) = kmeans(posMatrix, numGroups)
+
+centersVecArray = [Vec2(row[0], row[1]) for row in centroids]
+
+nodeGroup = assign_nearest_center(nodePos, centersVecArray)
+
+nx.draw(G, nodePos, node_color=nodeGroup, cmap=pylab.get_cmap('jet'))
 pylab.show()
--- vec2utils.py
+++ vec2utils.py
@@ -1,4 +1,80 @@
 
+import random
+from planar import Vec2
+
+class Int2:
+    x = 0
+    y = 0
+
+    def __init__(self, _x, _y):
+        self.x = _x
+        self.y = _y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def yieldtest(self):
+        yield 1
+        yield 2
+
+    def yield_4nbors(self):
+        """ in ccw order """
+        p = self
+        for i in range(4):
+            yield Int2(p.x+nbor4dx[i], p.y+nbor4dy[i])
+
+    def yield_8nbors(self):
+        """ in ccw order """
+        p = self
+        for i in range(8):
+            yield Int2(p.x+nbor8dx[i], p.y+nbor8dy[i])
+
+    def yield_8nbors_rand(self):
+        """ in random order """
+        p = self
+        for i in numpy.random.permutation(8):
+            yield Int2(p.x+nbor8dx[i], p.y+nbor8dy[i])
+
+    def astuple(self): return (self.x, self.y)
+
+class Grid:
+    W = 1
+    H = 1
+    grid = None
+
+    def __init__(self,_W, _H, default):
+        self.W = _W
+        self.H = _H
+        self.grid = range(self.W*self.H)
+        for i in range(self.W*self.H):
+            self.grid[i] = default
+
+    def check(self,p):
+        return p.x >= 0 and p.x < self.W and p.y >= 0 and p.y < self.H
+
+    def get(self,x,y):
+        return self.grid[self.W*y + x]
+
+    def pget(self,p):
+        return self.grid[self.W*p.y + p.x]
+
+    def set(self,x,y,value):
+        self.grid[self.W*y+x] = value
+
+    def pset(self,p,value):
+        self.grid[self.W*p.y+p.x] = value
+
+    def printgrid(self):
+        for y in range(self.H):
+            for x in range(self.W):
+                print self.get(x,y),
+            print ''
+
+    def iter(self):
+        for y in range(self.W):
+            for x in range(self.H):
+                yield (x,y)
+
 def vec2_dist(a,b): return (a-b).length
                     
 def assign_nearest_center(points, centers):
@@ -15,4 +91,43 @@ def assign_nearest_center(points, centers):
                 bestdist = dist
         assignment[i] = bestj
     return assignment
+
+def slow_poisson_sampler(mindist, numpoints):
+    points = []
+    while len(points) < numpoints:
+        while True:
+            p = Vec2(random.random(), random.random())
+            bad = False
+            for q in points:
+                if vec2_dist(p,q) < mindist:
+                    bad = True
+                    break
+            if not bad:
+                points += [p]
+                break
+    return points
+
+def distance_grid(poss):
+    N = len(poss)
+    D = Grid(N,N, 0.0)
+    for u in range(N):
+        for v in range(N):
+            pu = poss[u]
+            pv = poss[v]
+            D.set(u,v, (pu-pv).length)
+    return D
+
+def gaussian(x, a, b, c):
+    return a * math.exp(-1 * (x-b)*(x-b)/(2*c*c))
+
+class InterestCurve:
+    firstBump = 0.5
+    lastBump = 1.0
+    def eval(self, frac):
+        rv = 0
+        rv += self.firstBump * gaussian(frac-0.1, 0.10)
+        for center in [ 0.3, 0.5, 0.7]:
+            rv += (center*0.8) * 1.0 * gaussian(frac-center, 0.075)
+        rv += 1.0 * gaussian(frac-0.95, 0.1)
+        return rv
 
