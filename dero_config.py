18b28b7377a1840f7dbbafcb39c5c5fc8a95d8e7
Steven An <stevenan@Stevens-MacBook-Air.local>
2022-07-16 10:55:59 -0400

    fix dero wad paths..should not commit this

--- dero_config.py
+++ dero_config.py
@@ -1,8 +1,8 @@
 
 import os
 
-DOOM1_WAD_PATH = '/Users/Steve/Documents/Zandronum/DOOM.WAD'
-DOOM2_WAD_PATH = '/Users/Steve/Documents/Zandronum/DOOM2.WAD'
+DOOM1_WAD_PATH = '/Users/stevenan/dooming/wads/doom1/DOOM.WAD'
+DOOM2_WAD_PATH = '/Users/stevenan/dooming/wads/doom2/DOOM2.WAD'
 
 def build_wad( srcwad, destwad ):
     os.system( '/Users/Steve/bsp-5.2/bsp %s -o %s' % (srcwad, destwad) )
